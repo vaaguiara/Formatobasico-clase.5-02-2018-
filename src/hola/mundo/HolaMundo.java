235d9effd0ba212152286fd0362d39ce5e21ca17
Estudiante <Estudiante@10.203.0.189>
2018-02-05 08:32:21 -0500

    Mi primera version

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+    /*
+     Se debe dar click derecho al archivo del programa y crear nueva clase
+    Las clases deben tener letras en mayusculas
+     */
+    public static void main(String[] args) {// Asi se escribe el main
+        
+        System.out.println("Bienvenidos POO");// Imprime, equivalente al cout
+    }
+    
+}
